b87c688401091ab476d8db79559596c111c83499
cyrloko <cyr.loko@yahoo.com>
2021-11-21 21:14:12 +0100

    Modification de la classe NflRareApplicationTests

--- src/test/java/fr/nflrae/NflrareApplicationTests.java
+++ src/test/java/fr/nflrae/NflrareApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NflrareApplicationTests {
+class NflRareApplicationTests {
 
 	@Test
 	void contextLoads() {
