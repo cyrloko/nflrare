d8a96f2ddeb0caf94537b208d6c3d41e8fe1fefe
cyloko <cyr.loko@capgemini.com>
2021-11-21 16:04:15 +0100

    Init

---
+++ src/main/java/fr/nflrae/NflrareApplication.java
@@ -0,0 +1,13 @@
+package fr.nflrae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NflrareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NflrareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/nflrae/NflrareApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nflrae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NflrareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
