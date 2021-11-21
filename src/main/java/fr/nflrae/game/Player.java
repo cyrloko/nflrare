c7a387c850b2cfa04219e6b862ae1a8478f51063
cyrloko <cyr.loko@yahoo.com>
2021-11-21 21:34:18 +0100

    Mise a jour des imports

--- src/main/java/fr/nflrae/game/Player.java
+++ src/main/java/fr/nflrae/game/Player.java
@@ -9,9 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.TableGenerator;
-import java.util.UUID;
-import java.util.concurrent.ThreadLocalRandom;
 
 @Data
 @Entity
